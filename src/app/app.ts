3d2b4615852329446ac0b59999333de94338a921
labstraction <labstraction@protonmail.com>
2026-02-26 10:56:22 +0100

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FirebaseService } from './services/firebase-service';
+import { HeaderComponent } from "./components/header-component/header-component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('AngularFeedReader');
+
+  firebase = inject(FirebaseService);
+
+  constructor(){
+    console.log(this.firebase.app)
+  }
 }
