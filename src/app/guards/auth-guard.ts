6c47b24cc06dca636376c986bec4aab4d5bbed8c
Eve0784 <everceymedina@gmail.com>
2026-03-03 10:02:11 +0100

    modifiche auth guard

--- src/app/guards/auth-guard.ts
+++ src/app/guards/auth-guard.ts
@@ -1,21 +1,24 @@
-import { effect, inject } from '@angular/core';
+import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth-service';
+import { onAuthStateChanged } from 'firebase/auth';
 
 export const authGuard: CanActivateFn = (route, state) => {
- const authServ= inject(AuthService);
- const router = inject(Router);
- return new Promise((resolve, reject)=>{
-  // if (!authServ) {
-  //   reject(false);
-  // }
-  effect(()=>{
-    if (authServ.isAuth()) {
-      resolve(true);
-    } else{
-      router.navigate(['/login']);
-      resolve(false);
-    }
+  const authServ = inject(AuthService);
+  const router = inject(Router);
+  return new Promise((resolve, _) => {
+    // if (!authServ) {
+    //   reject(false);
+    // }
+
+    onAuthStateChanged(authServ.auth, (user) => {
+      if (user) {
+        resolve(true);
+      } else {
+       router.navigate(['/login']);
+       resolve(false)
+      }
+    });
+
   })
- })
 };
