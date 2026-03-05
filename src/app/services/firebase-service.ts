067d3698116e7b92b6126c4975423c16709cda06
Eve0784 <everceymedina@gmail.com>
2026-03-05 09:34:31 +0100

    environment

--- src/app/services/firebase-service.ts
+++ src/app/services/firebase-service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { FirebaseApp, initializeApp } from "firebase/app";
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -7,20 +8,9 @@ import { FirebaseApp, initializeApp } from "firebase/app";
 export class FirebaseService {
 
 
-  firebaseConfig = {
-  apiKey: "AIzaSyDWLLFJWOt6rsaWc_zYRjhGDy8RmzHbA2M",
-  authDomain: "feed-reader-56f2d.firebaseapp.com",
-  projectId: "feed-reader-56f2d",
-  storageBucket: "feed-reader-56f2d.firebasestorage.app",
-  messagingSenderId: "252825040713",
-  appId: "1:252825040713:web:485c44eee55ef839730c98"
-};
-
   app: FirebaseApp;
 
   constructor() {
-    this.app = initializeApp(this.firebaseConfig);
+    this.app = initializeApp(environment.firebaseConfig);
   }
-
-
 }
