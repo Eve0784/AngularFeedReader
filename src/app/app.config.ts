f12515d01071d9b05603554ac11542a7fa7f8ea1
Eve0784 <everceymedina@gmail.com>
2026-03-05 12:26:27 +0100

    dockfile

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,14 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes, withHashLocation())
   ]
 };
+
+
+// with hash locations si usa quando i webserver non sono configurati per leggere le single-page applications
